44c93bc894dd2534ad64c98d3f4b9642fcdc8d37
Carlos Chulo <cchulo.dev@gmail.com>
2023-08-05 19:02:09 -0700

    cleaned up conan, adding vcpkg to try it out

--- scripts/restore-conan-deps.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-~/.local/bin/conan profile detect --force
-
-~/.local/bin/conan install . \
-  -of build/conan \
-  --build=missing \
-  -s build_type=Release \
-  -s compiler.cppstd=gnu20
