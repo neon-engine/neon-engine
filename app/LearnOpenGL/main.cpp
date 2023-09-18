2e1f97294c38eccec10388d6b326fa52b12d8c6b
Carlos Chulo <cchulo.dev@gmail.com>
2023-09-18 03:52:53 -0700

    Can now render a rectangle

--- app/LearnOpenGL/main.cpp
+++ app/LearnOpenGL/main.cpp
@@ -65,9 +65,14 @@ int main() {
 
   // triangle
   float vertices[] = {
-      -0.5f, -0.5f, 0.0f,
-      0.5f, -0.5f, 0.0f,
-      0.0f, 0.5f, 0.0f
+      0.5f, 0.5f, 0.0f,  // top right
+      0.5f, -0.5f, 0.0f,  // bottom right
+      -0.5f, -0.5f, 0.0f,  // bottom left
+      -0.5f, 0.5f, 0.0f   // top left
+  };
+  unsigned int indices[] = {  // note that we start from 0!
+      0, 1, 3,   // first triangle
+      1, 2, 3    // second triangle
   };
 
   unsigned int vertex_shader, fragment_shader;
@@ -113,19 +118,30 @@ int main() {
   glDeleteShader(vertex_shader);
   glDeleteShader(fragment_shader);
 
-  unsigned int vbo, vao;
+  unsigned int vao, vbo, ebo;
   glGenVertexArrays(1, &vao);
   glGenBuffers(1, &vbo);
+  glGenBuffers(1, &ebo);
   glBindVertexArray(vao);
   glBindBuffer(GL_ARRAY_BUFFER, vbo);
   glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *) 0);
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *) nullptr);
   glEnableVertexAttribArray(0);
 
   while (!glfwWindowShouldClose(window)) {
     // input
     ProcessInput(window);
+
+    // If we ever cared to render in wireframe the following code would enable us to toggle it on and off
+    // if (wireframe) {
+    //   glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+    // } else {
+    //   glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+    // }
 
     // rendering commands
     glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
@@ -133,7 +149,7 @@ int main() {
 
     glUseProgram(shader_program);
     glBindVertexArray(vao);
-    glDrawArrays(GL_TRIANGLES, 0, 3);
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
 
     // check and call events and swap the buffers
     glfwSwapBuffers(window);
@@ -143,4 +159,3 @@ int main() {
   glfwTerminate();
   return 0;
 }
-
