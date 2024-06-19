10117d059aa382f5641f377f6c64065298f08e2a
Carlos Chulo <cchulo.dev@gmail.com>
2024-06-19 03:29:40 -0700

    started working on a generic app as part of the core library. Regretted 4 spaces, will revert to 2 spaces

--- app/NeonFPS/main.cpp
+++ app/NeonFPS/main.cpp
@@ -1,5 +1,17 @@
 
+#include <iostream>
+#include <stdexcept>
+#include <core/application.hpp>
+
 int main()
 {
-    return 0;
+    core::Application app{};
+
+    try
+    {
+        app.Run();
+    } catch (const std::exception &e)
+    {
+        std::cerr << e.what() << std::endl;
+    }
 }
---
+++ lib/core/application.hpp
@@ -0,0 +1,13 @@
+#ifndef APPLICATION_HPP
+#define APPLICATION_HPP
+
+namespace core {
+
+class Application {
+public:
+    void Run();
+};
+
+} // core
+
+#endif //APPLICATION_HPP
