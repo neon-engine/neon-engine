684a67fcfa9b03a76bf76a99360213fab2227d52
Carlos Chulo <cchulo.dev@gmail.com>
2025-03-02 08:50:22 -0800

    created more robusts scripts for syncing asset files

---
+++ cmake/scripts/CheckAndCopy.cmake
@@ -0,0 +1,30 @@
+# Usage:
+#   cmake -D SRC="path/to/source.file" -D DST="path/to/dest.file" -P CheckAndCopy.cmake
+
+if (NOT DEFINED SRC)
+    message(FATAL_ERROR "SRC variable not defined!")
+endif ()
+if (NOT DEFINED DST)
+    message(FATAL_ERROR "DST variable not defined!")
+endif ()
+
+if ((NOT EXISTS "${SRC}") AND (NOT EXISTS "${DST}"))
+    message(STATUS "Both source ${SRC} and destination ${DST} do not exist. Skipping.")
+    return()
+endif ()
+
+if (NOT EXISTS "${DST}")
+    message(STATUS "Copying ${SRC} -> ${DST} (destination not found)")
+    execute_process(COMMAND "${CMAKE_COMMAND}" -E copy "${SRC}" "${DST}")
+    return()
+endif ()
+
+file(SHA256 "${SRC}" SRC_HASH)
+file(SHA256 "${DST}" DST_HASH)
+
+if (NOT "${SRC_HASH}" STREQUAL "${DST_HASH}")
+    message(STATUS "Copying ${SRC} -> ${DST} (hash mismatch)")
+    execute_process(COMMAND "${CMAKE_COMMAND}" -E copy "${SRC}" "${DST}")
+else ()
+    message(STATUS "Skipping copy of ${SRC} (no changes)")
+endif ()
---
+++ cmake/scripts/RemoveOrphans.cmake
@@ -0,0 +1,27 @@
+# Usage:
+#   cmake -D SOURCE_ASSETS_DIR="..." -D OUTPUT_ASSETS_DIR="..." -P RemoveOrphans.cmake
+
+if (NOT DEFINED SOURCE_ASSETS_DIR)
+    message(FATAL_ERROR "SOURCE_ASSETS_DIR variable not defined!")
+endif ()
+if (NOT DEFINED OUTPUT_ASSETS_DIR)
+    message(FATAL_ERROR "OUTPUT_ASSETS_DIR variable not defined!")
+endif ()
+
+message(STATUS "Checking for orphaned files in: ${OUTPUT_ASSETS_DIR}")
+
+file(GLOB_RECURSE CURRENT_TARGET_FILES
+        "${OUTPUT_ASSETS_DIR}/*"
+)
+
+list(FILTER CURRENT_TARGET_FILES EXCLUDE REGEX "\\.dvc$")
+
+foreach (TGT_FILE IN LISTS CURRENT_TARGET_FILES)
+    file(RELATIVE_PATH TGT_FILE_REL "${OUTPUT_ASSETS_DIR}" "${TGT_FILE}")
+
+    set(SRC_FILE "${SOURCE_ASSETS_DIR}/${TGT_FILE_REL}")
+    if (NOT EXISTS "${SRC_FILE}")
+        message(STATUS "Removing orphaned file: ${TGT_FILE}")
+        execute_process(COMMAND ${CMAKE_COMMAND} -E remove "${TGT_FILE}")
+    endif ()
+endforeach ()
